ee4ba10fc7968e346683779596c525761bcb6312
Zuli Dhaduk <juli.d@dibc.in>
2020-11-30 14:32:48 +0530

    initial commit

---
+++ app/src/main/java/in/dibc/kidharhai/SplashActivity.java
@@ -0,0 +1,101 @@
+package in.dibc.kidharhai;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.bumptech.glide.Glide;
+import in.dibc.kidharhai.R;
+
+import in.dibc.kidharhai.utils.Constants;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = "SplashActivity";
+    private ImageView splashImage;
+    private String[] locationPermission = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        splashImage = findViewById(R.id.splash_image);
+        Glide.with(SplashActivity.this).load(R.drawable.logo).into(splashImage);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Constants.checkLocationPermission(this)) {
+                redirect(3000);
+            } else {
+                ActivityCompat.requestPermissions(this, locationPermission, 11);
+            }
+        } else {
+            redirect(3000);
+        }
+    }
+
+    private void redirect(long millis) {
+        SharedPreferences prefs = getSharedPreferences(Constants.LOGIN_PREFS, MODE_PRIVATE);
+        String authKey = prefs.getString(Constants.AUTH_KEY, null);
+        String secret = prefs.getString(Constants.SECRET, null);
+
+        Log.d(TAG, "redirect: authKey => " + authKey);
+        Log.d(TAG, "redirect: secret => " + secret);
+
+        new Handler().postDelayed(() -> {
+            if (authKey == null || secret == null) {
+                Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            } else {
+                Intent dashIntent = new Intent(SplashActivity.this, DashboardActivity.class);
+                startActivity(dashIntent);
+            }
+            finish();
+        }, millis);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 11) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                redirect(1000);
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, locationPermission[0])){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Location Permission");
+                    builder.setMessage("It is important to have location permission to function the app properly. Please allow it to access all features");
+                    builder.setPositiveButton("GIVE PERMISSION", (dialogInterface, i) -> {
+                        ActivityCompat.requestPermissions(this, locationPermission, 11);
+                    });
+                    builder.setNegativeButton("DISMISS",  (dialogInterface, i) -> {
+                        finish();
+                    });
+                    builder.create().show();
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
---
+++ app/src/main/java/in/dibc/kidharhai/models/MResVerify.java
@@ -0,0 +1,44 @@
+package in.dibc.kidharhai.models;
+
+import com.squareup.moshi.Json;
+
+public class MResVerify{
+
+	@Json(name = "msg")
+	private String msg;
+
+	@Json(name = "authkey")
+	private String authkey;
+
+	@Json(name = "clientid")
+	private String clientid;
+
+	@Json(name = "secret")
+	private String secret;
+
+	public String getMsg(){
+		return msg;
+	}
+
+	public String getAuthkey(){
+		return authkey;
+	}
+
+	public String getClientid(){
+		return clientid;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"MResVerify{" + 
+			"msg = '" + msg + '\'' + 
+			",authkey = '" + authkey + '\'' + 
+			",clientid = '" + clientid + '\'' + 
+			"}";
+		}
+}
